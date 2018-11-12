d76d1924cc3dd527984fa4caeeb7810a3ee017b2
p-olsen <44850858+p-olsen@users.noreply.github.com>
2018-11-12 16:43:53 -0700

    testing logins

--- testPOS/Login.xaml.cs
+++ testPOS/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +21,42 @@ namespace testPOS
     /// </summary>
     public partial class Login : Window
     {
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+
+        PosDBEntities posEBEntities = new PosDBEntities();
+        ObservableCollection<User> users;
+
         public Login()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            users = new ObservableCollection<User>(posEBEntities.Users);
+            foreach(User user in users)
+            {
+                user.Username.Trim();
+            }
+
+        }
+
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
+            var query = from user in users
+                        where (UsernameTB.Text == user.Username && PasswordTB.Text == user.Password)
+                        select user;
             //check for correct login info
-            MainWindow window = new MainWindow();
-            window.Show();
-            this.Close();
+            if(query.Count() > 0)
+            {
+                MainWindow window = new MainWindow();
+                window.Show();
+                this.Close();
+            }
+            
         }
+
+        
     }
 }
